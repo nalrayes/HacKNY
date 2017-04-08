350b1f5096a602985842f42199c49754ae37121d
Tarik <tz638@nyu.edu>
2017-04-08 19:20:47 -0400

    Chrome extension that opens five new tabs.

---
+++ FiveTabExtension/content.js
@@ -0,0 +1,19 @@
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      if ($("a[href^='http']").length>5)	{
+
+      	var i;
+      	var firstHref=new Array(5);
+      	for (i=0; i<5; i++)	{
+
+      		firstHref[i]=($("a[href^='http']").eq(i).attr("href"));
+      	}           	
+
+      	// This line is new!
+      	chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+      }      
+    }
+  }
+);
