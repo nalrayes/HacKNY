afadd5fcf8cc53ddae34b2b96a7b0d1507cfcd7d
Matthew Russo <mcr431@nyu.edu>
2017-04-08 18:30:25 -0400

    modified sample.js for http get requests

--- event_page/sample.js
+++ event_page/sample.js
@@ -1,8 +1,25 @@
-// The onClicked callback function.
+var jsonResponse;
+var currentURL;
+var WebhoseAPIKey = "58b8f3b8-5631-4a3d-93fd-83bdd4e83155";
+
+var WebhoseAPISearchURL = "https://webhose.io/search?token={0}&format=json&q=thread.url:{1}".format(WebhoseAPIKey,currentURL);
+
+var WebhosesearchTerms = "NULL";
+var WebhoseAPIEndpoint = "http://webhose.io/search?token={0}&format=json&q=thread.title%3A({1})%20language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy".format(WebhoseAPIKey,searchTerms);
+
+
+
+
+var GoogleNatLangAPIKey = "AIzaSyDhbtlZtzf7AdRfMjntRCGTCsvYgFBog8g";
+var GoogleNatLangAPIBaseURL = "https://language.googleapis.com/v1beta1/documents:analyzeEntities";
+
+
 function onClickHandler(info, tab) {
-    console.log("item " + info.menuItemId + " was clicked");
-    console.log("info: " + info.linkUrl);
-    console.log("tab: " + JSON.stringify(tab));
+	currentURL = info.linkUrl;
+	
+	httpGetAsync(currentURL);
+	
+	console.log("JSON response from {0} link : ".format(currentURL) + jsonResponse);
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
@@ -13,6 +30,24 @@ chrome.runtime.onInstalled.addListener(function() {
     var id = chrome.contextMenus.create({"title": title, "contexts":["link"],
                                          "id": "contextlinkayy"});
     console.log("'linkayy' item:" + id);
-
-
 });
+
+
+
+// Basic HTTPGetAsyncMethod
+function httpGetAsync(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200){
+			jsonResponse = JSON.parse(this.responseText);
+		}
+    };
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send();
+}
+
+
+
+
+
