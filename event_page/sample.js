16a14810926e4dbf0bb8ed4ff35ec285bd4a05ef
Matthew Russo <mcr431@nyu.edu>
2017-04-08 19:22:03 -0400

    Click on menu item properly returns title of article

--- event_page/sample.js
+++ event_page/sample.js
@@ -1,13 +1,13 @@
 var jsonResponse;
+var title;
 var currentURL;
+var hasJSONArrived = false;
 var WebhoseAPIKey = "58b8f3b8-5631-4a3d-93fd-83bdd4e83155";
 
-var WebhoseAPISearchURL = "https://webhose.io/search?token={0}&format=json&q=thread.url:{1}".format(WebhoseAPIKey,currentURL);
+var WebhoseAPISearchURL = new String('https://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.url:' + currentURL);
 
 var WebhosesearchTerms = "NULL";
-var WebhoseAPIEndpoint = "http://webhose.io/search?token={0}&format=json&q=thread.title%3A({1})%20language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy".format(WebhoseAPIKey,searchTerms);
-
-
+//var WebhoseAPIEndpoint = new String('http://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.title%3A(' + searchTerms + ')%20language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy');
 
 
 var GoogleNatLangAPIKey = "AIzaSyDhbtlZtzf7AdRfMjntRCGTCsvYgFBog8g";
@@ -16,11 +16,17 @@ var GoogleNatLangAPIBaseURL = "https://language.googleapis.com/v1beta1/documents
 
 function onClickHandler(info, tab) {
 	currentURL = info.linkUrl;
+	if (currentURL.includes("http://")) {
+		console.log("working");
+		currentURL = currentURL.replace("http://","http\\:\\/\\/");
+	}
+	else if (currentURL.includes("https://")){
+		currentURL = currentURL.replace("https://","https\\:\\/\\/");
+	}
 	
-	httpGetAsync(currentURL);
-	
-	console.log("JSON response from {0} link : ".format(currentURL) + jsonResponse);
-};
+	WebhoseAPIKey = new String('https://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.url:' + currentURL);
+	httpGetAsync(WebhoseAPIKey);	
+}
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
@@ -29,18 +35,19 @@ chrome.runtime.onInstalled.addListener(function() {
     var title = "Test link ayy menu item";
     var id = chrome.contextMenus.create({"title": title, "contexts":["link"],
                                          "id": "contextlinkayy"});
-    console.log("'linkayy' item:" + id);
 });
 
 
 
 // Basic HTTPGetAsyncMethod
-function httpGetAsync(theUrl)
+function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200){
-			jsonResponse = JSON.parse(this.responseText);
+			jsonResponse = JSON.parse(xmlHttp.responseText);
+			title = jsonResponse['posts'][0]['thread']['title'];
+			callback(title);
 		}
     };
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
