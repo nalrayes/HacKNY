2651ec20c43c0854284496cb9b0c2b83df9db092
Matthew Russo <mcr431@nyu.edu>
2017-04-08 20:27:31 -0400

    grab search terms function

--- event_page/sample.js
+++ event_page/sample.js
@@ -1,7 +1,7 @@
 var jsonResponse;
 var title;
 var currentURL;
-<<<<<<< 16a14810926e4dbf0bb8ed4ff35ec285bd4a05ef
+
 var hasJSONArrived = false;
 var WebhoseAPIKey = "58b8f3b8-5631-4a3d-93fd-83bdd4e83155";
 
@@ -9,7 +9,7 @@ var WebhoseAPISearchURL = new String('https://webhose.io/search?token=' + Webhos
 
 var WebhosesearchTerms = "NULL";
 //var WebhoseAPIEndpoint = new String('http://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.title%3A(' + searchTerms + ')%20language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy');
-=======
+
 // var WebhoseAPIKey = "58b8f3b8-5631-4a3d-93fd-83bdd4e83155";
 
 // var WebhoseAPISearchURL = "https://webhose.io/search?token={0}&format=json&q=thread.url:{1}".format(WebhoseAPIKey,currentURL);
@@ -17,8 +17,6 @@ var WebhosesearchTerms = "NULL";
 // var WebhosesearchTerms = "NULL";
 // var WebhoseAPIEndpoint = "http://webhose.io/search?token={0}&format=json&q=thread.title%3A({1})%20language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy".format(WebhoseAPIKey,searchTerms);
 
-
->>>>>>> google authentication working
 
 
 var GoogleNatLangAPIKey = "AIzaSyDhbtlZtzf7AdRfMjntRCGTCsvYgFBog8g";
@@ -36,11 +34,11 @@ function onClickHandler(info, tab) {
 	}
 	
 	WebhoseAPIKey = new String('https://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.url:' + currentURL);
-	httpGetArticleTitle(WebhoseAPIKey);	
+	httpGetArticleTitle(WebhoseAPIKey,getKeywords);	
 	
 	WebhoseAPIEndpoint = new String('http://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.title%3A(syria%20%trump)%20language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy');
 	
-	httpGetFiveArticlesFromSearchTerms(WebhoseAPIEndpoint);
+	//httpGetFiveArticlesFromSearchTerms(WebhoseAPIEndpoint);
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
@@ -50,28 +48,32 @@ chrome.runtime.onInstalled.addListener(function() {
     var title = "Test link ayy menu item";
     var id = chrome.contextMenus.create({"title": title, "contexts":["link"],
                                          "id": "contextlinkayy"});
-<<<<<<< 16a14810926e4dbf0bb8ed4ff35ec285bd4a05ef
-=======
+
     console.log("'linkayy' item:" + id);
-    getKeywords();
->>>>>>> google authentication working
+    //getKeywords();
 });
 
-function getKeywords() {
+function getKeywords(title) {
     xhr = new XMLHttpRequest();
     xhr.open("POST", GoogleNatLangAPIBaseURL, true);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function () { 
         if (xhr.readyState == 4 && xhr.status == 200) {
             var json = JSON.parse(xhr.responseText);
-            console.log(json)
+            console.log(json);
+			var searchTerms = [];
+			var item;
+			for (item = 0; item < json.entities.length; item++) {
+				searchTerms.push(json.entities[item]['name'].replace(" ", "%20"));
+			}
+			console.log(searchTerms);
         }
     }
     var data = JSON.stringify({
           "encodingType": "UTF8",
           "document": {
             "type": "PLAIN_TEXT",
-            "content": "The Emerging Trump Doctrine: Don’t Follow Doctrine",
+            "content": title,
           }
         }
     );
@@ -81,7 +83,7 @@ function getKeywords() {
 
 
 // Basic HTTPGetAsyncMethod
-function httpGetArticleTitle(theUrl)
+function httpGetArticleTitle(theUrl, callback)
 {
 
     var xmlHttp = new XMLHttpRequest();
@@ -89,7 +91,7 @@ function httpGetArticleTitle(theUrl)
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200){
 			jsonResponse = JSON.parse(xmlHttp.responseText);
 			title = jsonResponse['posts'][0]['thread']['title'];
-			//callback(title);
+			callback(title);
 		}
     };
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
