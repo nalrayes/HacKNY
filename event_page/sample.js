a0ce61273429a2a8374d0f0c5ddceaebdc6b315d
Matthew Russo <mcr431@nyu.edu>
2017-04-09 02:32:06 -0400

    Adds the visualmenu i was working on. pls read slack message.

--- event_page/sample.js
+++ event_page/sample.js
@@ -7,18 +7,6 @@ var WebhoseAPIKey = "58b8f3b8-5631-4a3d-93fd-83bdd4e83155";
 
 var WebhoseAPISearchURL = new String('https://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.url:' + currentURL);
 
-var WebhosesearchTerms = "NULL";
-//var WebhoseAPIEndpoint = new String('http://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.title%3A(' + searchTerms + ')%20language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy');
-
-// var WebhoseAPIKey = "58b8f3b8-5631-4a3d-93fd-83bdd4e83155";
-
-// var WebhoseAPISearchURL = "https://webhose.io/search?token={0}&format=json&q=thread.url:{1}".format(WebhoseAPIKey,currentURL);
-
-// var WebhosesearchTerms = "NULL";
-// var WebhoseAPIEndpoint = "http://webhose.io/search?token={0}&format=json&q=thread.title%3A({1})%20language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy".format(WebhoseAPIKey,searchTerms);
-
-
-
 var GoogleNatLangAPIKey = "AIzaSyDhbtlZtzf7AdRfMjntRCGTCsvYgFBog8g";
 var GoogleNatLangAPIBaseURL = "https://language.googleapis.com/v1beta1/documents:analyzeEntities?key=" + "AIzaSyDhbtlZtzf7AdRfMjntRCGTCsvYgFBog8g";
 
@@ -34,11 +22,13 @@ function onClickHandler(info, tab) {
 	}
 	
 	WebhoseAPIKey = new String('https://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.url:' + currentURL);
-	httpGetArticleTitle(WebhoseAPIKey,getKeywords);	
+	
+	httpGetArticleTitle(WebhoseAPIKey,getKeywords);
 	
 	WebhoseAPIEndpoint = new String('http://webhose.io/search?token=' + WebhoseAPIKey + '&format=json&q=thread.title%3A(syria%20%trump)%20language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy');
 	
-	//httpGetFiveArticlesFromSearchTerms(WebhoseAPIEndpoint);
+	// SHOW DISPLAY BOX
+	//
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
@@ -46,11 +36,7 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 // Set up context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function() {
     var title = "Test link ayy menu item";
-    var id = chrome.contextMenus.create({"title": title, "contexts":["link"],
-                                         "id": "contextlinkayy"});
-
-    console.log("'linkayy' item:" + id);
-    //getKeywords();
+    var id = chrome.contextMenus.create({"title": title, "contexts":["link"],"id": "contextlinkayy"});
 });
 
 function getKeywords(title) {
@@ -60,13 +46,25 @@ function getKeywords(title) {
     xhr.onreadystatechange = function () { 
         if (xhr.readyState == 4 && xhr.status == 200) {
             var json = JSON.parse(xhr.responseText);
-            console.log(json);
 			var searchTerms = [];
 			var item;
 			for (item = 0; item < json.entities.length; item++) {
-				searchTerms.push(json.entities[item]['name'].replace(" ", "%20"));
+				var substr = json.entities[item]['name'].split(" ");
+				for (var i = 0; i < substr.length; i++){
+					searchTerms.push(substr + "%20");
+				}
 			}
-			console.log(searchTerms);
+			
+			var url = "http://webhose.io/search?token=58b8f3b8-5631-4a3d-93fd-83bdd4e83155&format=json&q=";
+			for (item = 0; item < searchTerms.length; item++){
+				if (item > 2) {
+					break;
+				}
+				url = url + searchTerms[item];
+			}
+			url = url + "language%3A(english)%20performance_score%3A%3E1%20(site_type%3Anews)&sort=relevancy";
+			
+			httpGetFiveArticlesFromSearchTerms(url);
         }
     }
     var data = JSON.stringify({
@@ -85,7 +83,6 @@ function getKeywords(title) {
 // Basic HTTPGetAsyncMethod
 function httpGetArticleTitle(theUrl, callback)
 {
-
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200){
@@ -103,16 +100,44 @@ function httpGetFiveArticlesFromSearchTerms(theUrl){
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200){
 			jsonResponse = JSON.parse(xmlHttp.responseText);
+			
+			var imageArray = [];
+			var siteNameArray = [];
+			var linkArray = [];
+			
+			console.log(jsonResponse);
+			
 			// Logic for selecting 5 articles
 			// We will have a json object of lots of different articles
-			console.log("RESULTS FROM SEARCH TERMS: " + jsonResponse);
+			// Currently just pulls info of top 5 responses
+			for(var i = 0; i < 5; i++){
+				imageArray.push(jsonResponse['posts'][i]['thread']['main_image']); // adds the image
+				siteNameArray.push(jsonResponse['posts'][i]['thread']['site']); // adds the site name
+				linkArray.push(jsonResponse['posts'][i]['url']) // adds the link
+			}
+			
+			// Sends the information to the Chrome Extension Pop Up (when you click icon next to search bar)
+			modifyPopUp(imageArray, siteNameArray, linkArray);
 		}
     };
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send();
 }
 
-
-
-
+// SUPER JANKY AND NEEDS A PERMANENT SOLUTION BECAUSE ONLY WORKS IF THE POP UP WINDOW IS OPEN WHEN API REQUEST IS SENT
+// NEED TO FIND A WAY FOR A SCRIPT ON THIS PAGE TO TALK WITH SCRIPT ON POP UP PAGE.
+// IDEALLY THESE THINGS WOULD BE FILES ON AN ACTUAL HOSTED SERVER AND WOULD JUST PUSH UPDATES TO THE BROSWER
+//
+// Sends a message to the linksPopUp.js script attached to the Chrome Extension Pop Up
+// Sends an array of 5 images, 5 links, and 5 site names
+//
+function modifyPopUp(arrayOfSiteImages, arrayOfSiteNames, arrayOfLinks) {
+  chrome.runtime.sendMessage({
+  from:    'sample',
+  subject: 'changeInfo',
+  imageArray: arrayOfSiteImages,
+  siteNameArray: arrayOfSiteNames,
+  linkArray: arrayOfLinks
+});
+}
 
